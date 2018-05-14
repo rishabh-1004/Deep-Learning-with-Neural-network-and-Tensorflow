f41064d6fedbc0e8c19132421d7236b9d710e3be
Rishabh <rishabhrajshrm00@gmail.com>
2018-05-14 19:03:54 +0530

    New Commit

---
+++ Making_Neural_Network_with_new_data/create_sentiment_featureset.py
@@ -0,0 +1,13 @@
+import nltk 
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import numpy as np
+import random
+import pickle
+from collection import Counter
+
+lemmatizer = WordNetLemmatizer()
+hm_lines = 10000000
+
+
+
